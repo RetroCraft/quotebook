568603d6110388ac7ead2e08cb42661949c108ec
RetroCraft <retrocraft314@gmail.com>
2017-01-04 16:03:34 -0500

    Rework query.php to use functions
    Signed-off-by: RetroCraft <retrocraft314@gmail.com>

--- src/php/query.php
+++ src/php/query.php
@@ -8,10 +8,40 @@ if (isset($_POST["action"])):
 switch($_POST["action"]) {
 
   case "main":
-    if (!isset($_POST["filters:search"]) || !isset($_POST["filters:speaker"])) {
+    if (!isset($_POST["filters:search"]) || !isset($_POST["filters:speaker"]))
       fail("Missing parameters");
-    }
 
+    main($_POST["filters:search"], $_POST["filters:speaker"]);
+    break;
+
+  case "changepass":
+    if (!isset($_POST['currpass']) || !isset($_POST['newpass']))
+      fail("Missing parameters");
+
+    changepass($_POST['currpass'], $_POST['newpass'], $_SESSION["user"]["name"]);
+    break;
+    
+  case "people":
+    people();
+    break;
+
+  case "submit":
+    if (!isset($_POST['speaker']) || !isset($_POST['quote']) || 
+        !isset($_POST['context']) || !isset($_POST['timestamp']) || !isset($_POST['morestuff']))
+      fail("Missing parameters");
+
+    submit($_POST['speaker'], $_POST['quote'], $_POST['context'], $_POST['timestamp'], $_POST['morestuff'], $_SESSION['user']['name']);
+    break;
+
+  default:
+    fail("Unknown action");
+}
+
+else:
+  fail("Unspecified action");
+endif;
+
+function main($search, $speaker) {
     $out = '{"status": "success", "authors": [';
 
     try {
@@ -36,22 +66,22 @@ switch($_POST["action"]) {
                 AND status = "Approved"
                 AND (quote LIKE :quote OR context LIKE :quote)';
 
-    if ($_POST["filters:speaker"] == "---") {
-      $speaker = "%";
+    if ($speaker == "---") {
+      $speakerGlob = "%";
     } else {
-      $speaker = $_POST["filters:speaker"];
+      $speakerGlob = $speaker;
     }
 
-    if ($_POST["filters:search"] == "") {
-      $quote = "%";
+    if ($search == "") {
+      $searchGlob = "%";
     } else {
-      $quote = "%" . $_POST["filters:search"] . "%";
+      $searchGlob = "%$search%";
     }
 
     try {
       $stmt = $dbh->prepare($query);
-      $stmt->bindParam(":speaker", $speaker, PDO::PARAM_STR);
-      $stmt->bindParam(":quote", $quote, PDO::PARAM_STR);
+      $stmt->bindParam(":speaker", $speakerGlob, PDO::PARAM_STR);
+      $stmt->bindParam(":quote", $searchGlob, PDO::PARAM_STR);
       $stmt->setFetchMode(PDO::FETCH_ASSOC);
       $stmt->execute();
     } catch (PDOException $e) {
@@ -70,22 +100,16 @@ switch($_POST["action"]) {
     $out .= "]}";
 
     die($out);
-    break;
+}
 
-  case "changepass":
-    if (!isset($_POST['currpass']) || !isset($_POST['newpass'])) {
-      fail("Missing parameters");
-    }
-
+function changepass($currpass, $newpass, $name) {
     // check for user
     $query = "SELECT name FROM users WHERE name = :name AND pass = :pass LIMIT 1;";
-    $name = $_SESSION["user"]["name"];
-    $pass = $_POST["currpass"];
 
     try {
       $stmt = $dbh->prepare($query);
       $stmt->bindParam(":name", $name, PDO::PARAM_STR);
-      $stmt->bindParam(":pass", $pass, PDO::PARAM_STR);
+      $stmt->bindParam(":pass", $currpass, PDO::PARAM_STR);
       $stmt->setFetchMode(PDO::FETCH_ASSOC);
       $stmt->execute();
     } catch (PDOException $e) {
@@ -93,8 +117,7 @@ switch($_POST["action"]) {
     }
 
     if ($row = $stmt->fetch()) {
-      $newpass = $_POST["newpass"];
-
+      // Update password
       try {
         $stmt = $dbh->prepare("UPDATE users SET pass = :pass WHERE name = :name");
         $stmt->bindParam(":name", $name, PDO::PARAM_STR);
@@ -109,70 +132,47 @@ switch($_POST["action"]) {
     } else {
       fail("Incorrect current password, or something else wrong happened.");
     }
-
-    break;
-    
-  case "people":
-    try {
-      $stmt = $dbh->prepare("SELECT name FROM users;");
-      $stmt->execute();
-    } catch (PDOException $e) {
-      fail($e->getMessage());
-    }
-
-    $out = '{"status": "success", "people": [';
-
-    while ($row = $stmt->fetch()) {
-      $out .= '{"name": "' . $row["name"] . '"},';
-    }
-
-    $out = rtrim($out, ",");
-    $out .= "]}";
-
-    die($out);
-
-    break;
-
-  case "submit":
-    if (!isset($_POST['speaker']) || !isset($_POST['quote']) || 
-        !isset($_POST['context']) || !isset($_POST['timestamp']) || !isset($_POST['morestuff'])) {
-      fail("Missing parameters");
-    }
-
-    $query = 'INSERT INTO quotes (quote, context, morestuff, speaker, `date`, submittedby) 
-              VALUES (:quote, :context, :morestuff, :speaker, :timestamp, :submittedby)';
-
-    $speaker = $_POST['speaker'];
-    $quote = $_POST['quote'];
-    $context = $_POST['context'];
-    $timestamp = $_POST['timestamp'];
-    $morestuff = $_POST['morestuff'];
-    $submittedby = $_SESSION['user']['name'];
-    
-    try {
-      $stmt = $dbh->prepare($query);
-      $stmt->bindParam(":speaker", $speaker, PDO::PARAM_STR);
-      $stmt->bindParam(":quote", $quote, PDO::PARAM_STR);
-      $stmt->bindParam(":context", $context, PDO::PARAM_STR);
-      $stmt->bindParam(":timestamp", $timestamp, PDO::PARAM_STR);
-      $stmt->bindParam(":morestuff", $morestuff, PDO::PARAM_STR);
-      $stmt->bindParam(":submittedby", $submittedby, PDO::PARAM_STR);
-      $stmt->execute();
-    } catch (PDOException $e) {
-      fail($e->getMessage());
-    }
-
-    die('{"status": "success"}');
-
-    break;
-
-  default:
-    fail("Unknown action");
 }
 
-else:
-  fail("Unspecified action");
-endif;
+function people() {
+  try {
+    $stmt = $dbh->prepare("SELECT name FROM users;");
+    $stmt->execute();
+  } catch (PDOException $e) {
+    fail($e->getMessage());
+  }
+
+  $out = '{"status": "success", "people": [';
+
+  while ($row = $stmt->fetch()) {
+    $out .= '{"name": "' . $row["name"] . '"},';
+  }
+
+  $out = rtrim($out, ",");
+  $out .= "]}";
+
+  die($out);
+}
+
+function submit($speaker, $quote, $context, $timestamp, $morestuff, $submittedby) {
+  $query = 'INSERT INTO quotes (quote, context, morestuff, speaker, `date`, submittedby) 
+            VALUES (:quote, :context, :morestuff, :speaker, :timestamp, :submittedby)';
+
+  try {
+    $stmt = $dbh->prepare($query);
+    $stmt->bindParam(":speaker", $speaker, PDO::PARAM_STR);
+    $stmt->bindParam(":quote", $quote, PDO::PARAM_STR);
+    $stmt->bindParam(":context", $context, PDO::PARAM_STR);
+    $stmt->bindParam(":timestamp", $timestamp, PDO::PARAM_STR);
+    $stmt->bindParam(":morestuff", $morestuff, PDO::PARAM_STR);
+    $stmt->bindParam(":submittedby", $submittedby, PDO::PARAM_STR);
+    $stmt->execute();
+  } catch (PDOException $e) {
+    fail($e->getMessage());
+  }
+
+  die('{"status": "success"}');
+}
 
 function fail($error) {
   die('{"status": "error", "message": "' . $error . '", "$_POST": ' . json_encode($_POST) . ', "$_SESSION": ' . json_encode($_SESSION) . '}');
