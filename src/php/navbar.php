3fc0520762aaa99046316855d3dc0f9d657666f9
RetroCraft <retrocraft314@gmail.com>
2017-01-28 17:34:43 -0500

    Revamp navbar to look kewl

--- src/php/navbar.php
+++ src/php/navbar.php
@@ -1,23 +1,30 @@
 <main>
 <nav class="navbar">
   <div class="nav-wrapper" id="navbar">
-    <a class="brand-logo center" href="/">Quotebook</a>
-    <a href="#" data-activates="mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    <ul class="left hide-on-med-and-down">
-      <li><a href="/">Home</a></li>
-      <li><a href="/submit.php">Submit</a></li>
-      <li><a href="/dashboard.php">Dashboard</a></li>
-    </ul>
-    <ul class="right hide-on-med-and-down">
-      <li><a href="/settings.php"><i class="material-icons">settings</i></a></li>
-      <li><a href="/php/checklogin.php?logout"><i class="material-icons">exit_to_app</i></a></li>
-    </ul>
-    <ul class="side-nav" id="mobile">
-      <li><a href="/">Home</a></li>
-      <li><a href="/submit.php">Submit</a></li>
-      <li><a href="/dashboard.php">Dashboard</a></li>      
-      <li><a href="/settings.php">Settings</a></li>
-      <li><a href="/php/checklogin.php?logout">Logout</a></li>
-    </ul>
+  <a class="brand-logo center hide-on-small-and-down" href="/">Quotebook</a>
+  <ul class="left">
+    <li class="waves-effect"><a href="/">
+      <i class="material-icons left">home</i>
+      <span class="hide-on-med-and-down"> Home</span>
+    </a></li>
+    <li class="waves-effect"><a href="/submit.php">
+      <i class="material-icons left">create</i>
+      <span class="hide-on-med-and-down"> Submit</span>
+    </a></li>
+    <li class="waves-effect"><a href="/dashboard.php">
+      <i class="material-icons left">dashboard</i>
+      <span class="hide-on-med-and-down"> Dashboard</span>
+    </a></li>
+  </ul>
+  <ul class="right">
+    <li class="waves-effect"><a href="/settings.php">
+      <i class="material-icons left">settings</i>
+      <span class="hide-on-med-and-down"> Settings</span>
+    </a></li>
+    <li class="waves-effect"><a href="/php/checklogin.php?logout">
+      <i class="material-icons left">exit_to_app</i>
+      <span class="hide-on-med-and-down"> Logout</span>
+    </a></li>
+  </ul>
   </div>
 </nav>
