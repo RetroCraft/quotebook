fc8b9066416a24486aff60e39de9d21079c40ab5
RetroCraft <retrocraft314@gmail.com>
2017-02-05 20:40:03 -0500

    Gulp the JS things

--- src/php/header.php
+++ src/php/header.php
@@ -8,9 +8,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/css/tether.min.css">
 <link rel="stylesheet" href="/css/style.css">
 
-<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
 <script src="/js/main.js"></script>
 
 <title>JFSS Quotebook</title>
